6bbebee895903336d13004ae71082b041bdbe794
mattgellert <mattgellert@gmail.com>
2017-09-20 15:22:47 -0400

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,3 +1,50 @@
 class FiguresController < ApplicationController
 
+  get '/figures' do
+    @figures = Figure.all
+    erb :"figures/index"
+  end
+
+  get '/figures/new' do
+    erb :"figures/new"
+  end
+
+  post '/figures' do
+    @figure = Figure.create(params[:figure])
+    if !params[:landmark][:name].empty?
+      @landmark = Landmark.create(name: params[:landmark][:name])
+      @figure.landmarks << @landmark
+    end
+
+    if !params[:title][:name].empty?
+      @title = Title.create(name: params[:title][:name])
+      @figure.titles << @title
+    end
+    redirect to "/figures"
+  end
+
+  get '/figures/:id' do
+    @figure = Figure.find(params[:id])
+    erb :"figures/show"
+  end
+
+  get '/figures/:id/edit' do
+    @figure = Figure.find(params[:id])
+    erb :"figures/edit"
+  end
+
+  patch '/figures/:id' do
+    @figure = Figure.find(params[:id])
+    @figure.update(params[:figure])
+    if !params[:landmark][:name].empty?
+      @landmark = Landmark.create(name: params[:landmark][:name], year_completed: params[:landmark][:year_completed])
+      @figure.landmarks << @landmark
+    end
+    if !params[:title][:name].empty?
+      @title = Title.create(name: params[:title][:name])
+      @figure.titles << @title
+    end
+    redirect to "/figures/#{@figure.id}"
+  end
+
 end
