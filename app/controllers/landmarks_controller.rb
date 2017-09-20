59af9194c6aa786a54bee5e49f76f6f424070751
mattgellert <mattgellert@gmail.com>
2017-09-20 11:29:17 -0400

    add create new and read index routes

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,3 +1,33 @@
 class LandmarksController < ApplicationController
 
+  get '/landmarks' do
+    @landmarks = Landmark.all
+    erb :"landmarks/index"
+  end
+
+  get '/landmarks/new' do
+    erb :"landmarks/new"
+  end
+
+  post '/landmarks' do
+    Landmark.create(name: params[:name], year_completed: params[:year_completed])
+    redirect to "/landmarks"
+  end
+
+  get '/landmarks/:id' do
+    @landmark = Landmark.find(params[:id])
+    erb :"landmarks/show"
+  end
+
+  get '/landmarks/:id/edit' do
+    @landmark = Landmark.find(params[:id])
+    erb :"landmarks/edit"
+  end
+
+  patch '/landmarks/:id' do
+    @landmark = Landmark.find(params[:id])
+    @landmark.update(name: params[:name], year_completed: params[:year_completed])
+    redirect to "/landmarks/#{@landmark.id}"
+  end
+
 end
